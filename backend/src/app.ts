a3c6cd8f45098eed414c6776bfaaa513af876cb6
milenaa_052 <milenasantosdeoliveira40@gmail.com>
2025-09-24 14:52:22 -0300

    feat: creating service provider routes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,12 +1,14 @@
 import express from "express"
 import ProviderRoutes from "./routes/ProviderRoutes.js"
 import AddressRoutes from "./routes/AddressRoutes.js"
+import ServiceProviderRoutes from "./routes/ServiceProviderRoutes.js"
 
 const app = express()
 app.use(express.json())
 
 app.use('/api', ProviderRoutes);
 app.use('/api', AddressRoutes);
+app.use('/api', ServiceProviderRoutes)
 
 app.get("/", (req, res) => {
     res.send("Hello World!")
