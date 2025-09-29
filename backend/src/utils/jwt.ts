108f65b764eba78bf5cd8121146ab750ac9c70e3
AnaMariFranco <mariana.franco@grupointegrado.br>
2025-09-29 15:17:56 -0300

    fix: remove types from jwt

--- backend/src/utils/jwt.ts
+++ backend/src/utils/jwt.ts
@@ -4,10 +4,10 @@ import AdminModel from "../models/AdminModel.js"
 
 dotenv.config()
 
-const JWT_SECRET = process.env.JWT_SECRET as string
+const JWT_SECRET = process.env.JWT_SECRETS
 const JWT_EXPIRES_IN = "7d"
 
-export const generateToken = (Admin: AdminModel): string => {
+export const generateToken = (Admin)=> {
     const payload = {
         idAdmin: Admin.idAdmin,
         name: Admin.name,
@@ -18,11 +18,6 @@ export const generateToken = (Admin: AdminModel): string => {
     return jwt.sign(payload, JWT_SECRET, { expiresIn: JWT_EXPIRES_IN })
 }
 
-export const verifyToken = (token: string) => {
-    return jwt.verify(token, JWT_SECRET) as {
-        idAdmin: string;
-        name: string;
-        email: string;
-        AdminType: "ADMIN";
-    };
+export const verifyToken = (token) => {
+    return jwt.verify(token, JWT_SECRET) 
 }
