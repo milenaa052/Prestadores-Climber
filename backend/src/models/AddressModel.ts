8bf14479269e07e998506fa8a86cf74a10621d16
milenaa_052 <milenasantosdeoliveira40@gmail.com>
2025-10-02 15:13:28 -0300

    refactor: refactoring the address model

--- backend/src/models/AddressModel.ts
+++ backend/src/models/AddressModel.ts
@@ -1,33 +1,15 @@
 import { DataTypes, Model, Optional } from "sequelize"
 import sequelize from "../config/database.js"
 
-interface AddressAttributes {
-    idAddress: number
-    cep: string
-    state: string
-    city: string
-    neighborhood: string
-    street: string
-    number: number
-    complement?: string | null
-    createdAt?: Date;
-    updatedAt?: Date
-}
-
-interface AddressCreationAttributes extends Optional<AddressAttributes, "idAddress"> {}
-
-class AddressModel extends Model<AddressAttributes, AddressCreationAttributes>
-    implements AddressAttributes {
-    declare idAddress: number
-    declare cep: string
-    declare state: string
-    declare city: string
-    declare neighborhood: string
-    declare street: string
+class AddressModel extends Model {
+    declare idAddress: number;
+    declare cep: string;
+    declare state: string;
+    declare city: string;
+    declare neighborhood: string;
+    declare street: string;
     declare number: number
-    declare complement: string | null
-    declare readonly createdAt: Date
-    declare readonly updatedAt: Date
+    declare complement: string | null;
 }
 
 AddressModel.init({
